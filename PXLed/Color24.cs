983cc62b6e340a6e0ad87be8cb913f440bab8ea9
DanielPXL <bridan2005@t-online.de>
2022-01-23 14:54:13 +0100

    Add basic UI

---
+++ PXLed/Color24.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Windows.Media;
+
+namespace PXLed
+{
+    public struct Color24
+    {
+        /// <summary>
+        /// Creates Color24 from RGB
+        /// </summary>
+        /// <param name="r">Red component from 0 to 255</param>
+        /// <param name="g">Green component from 0 to 255</param>
+        /// <param name="b">Blue component from 0 to 255</param>
+        public Color24(byte r, byte g, byte b)
+        {
+            this.r = r;
+            this.g = g;
+            this.b = b;
+        }
+
+        public readonly byte r;
+        public readonly byte g;
+        public readonly byte b;
+
+        /// <summary>
+        /// Creates Color24 from RGB
+        /// </summary>
+        /// <param name="r">Red component from 0 to 255</param>
+        /// <param name="g">Green component from 0 to 255</param>
+        /// <param name="b">Blue component from 0 to 255</param>
+        public static Color24 FromRGB(byte r, byte g, byte b)
+        {
+            return new Color24(r, g, b);
+        }
+
+        // https://stackoverflow.com/a/1626232
+        /// <summary>
+        /// Creates Color24 from HSV
+        /// </summary>
+        /// <param name="hue">Hue from 0 to 360 degrees</param>
+        /// <param name="saturation">Saturation from 0 to 1</param>
+        /// <param name="value">Value from 0 to 1</param>
+        public static Color24 FromHSV(double hue, double saturation, double value)
+        {
+            int hi = Convert.ToInt32(Math.Floor(hue / 60)) % 6;
+            double f = hue / 60 - Math.Floor(hue / 60);
+
+            value *= 255;
+            byte v = Convert.ToByte(value);
+            byte p = Convert.ToByte(value * (1 - saturation));
+            byte q = Convert.ToByte(value * (1 - f * saturation));
+            byte t = Convert.ToByte(value * (1 - (1 - f) * saturation));
+
+            if (hi == 0)
+                return FromRGB(v, t, p);
+            else if (hi == 1)
+                return FromRGB(q, v, p);
+            else if (hi == 2)
+                return FromRGB(p, v, t);
+            else if (hi == 3)
+                return FromRGB(p, q, v);
+            else if (hi == 4)
+                return FromRGB(t, p, v);
+            else
+                return FromRGB(v, p, q);
+        }
+
+        public static Color24 Random()
+        {
+            Random r = new();
+            return FromRGB((byte)r.Next(0, 256), (byte)r.Next(0, 256), (byte)r.Next(0, 256));
+        }
+
+        public static implicit operator Color(Color24 c24)
+        {
+            return Color.FromRgb(c24.r, c24.g, c24.b);
+        }
+
+        public static implicit operator Color24(Color c)
+        {
+            return new Color24(c.R, c.G, c.B);
+        }
+    }
+}
---
+++ PXLed/LEDPreviewControl.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace PXLed
+{
+    /// <summary>
+    /// Displays a horizontal preview of LEDs using colored rectangles.
+    /// </summary>
+    public partial class LEDPreviewControl : UserControl
+    {
+        public LEDPreviewControl()
+        {
+            InitializeComponent();
+            SizeChanged += (object sender, SizeChangedEventArgs args) => ResizeContent(ActualWidth, ActualHeight);
+        }
+
+        Rectangle[]? ledRects;
+
+        public int LEDCount
+        {
+            get
+            {
+                return ledRects?.Length ?? 0;
+            }
+
+            set
+            {
+                // Reconstruct rects when changing number of displayed leds
+                // Remove old rects
+                panel.Children.Clear();                
+
+                // Add new rects
+                ledRects = new Rectangle[value];
+                Random r = new Random();
+                for (int i = 0; i < value; i++)
+                {
+                    ledRects[i] = new Rectangle();
+                    panel.Children.Add(ledRects[i]);
+
+                    // Make rect color random for debug
+                    ledRects[i].Fill = new SolidColorBrush(Color24.Random());
+                }
+
+                // Resize new rects
+                ResizeContent(ActualWidth, ActualHeight);
+            }
+        }
+
+        public void SetColors(ref Color24[] colors)
+        {
+            // If colors in array is not equal to number of rects, reconstruct the rects
+            if (colors.Length != ledRects?.Length)
+                LEDCount = colors.Length;
+
+            if (ledRects != null)
+            {
+                for (int i = 0; i < colors.Length; i++)
+                {
+                    ledRects[i].Fill = new SolidColorBrush(colors[i]);
+                }
+            }
+        }
+
+        void ResizeContent(double width, double height)
+        {
+            for (int i = 0; i < ledRects?.Length; i++)
+            {
+                ledRects[i].Width = width / ledRects.Length;
+                ledRects[i].Height = height;
+            }
+        }
+    }
+}
