25eb963f54b2a3139b93fe14f291bf4efc6e4279
DanielPXL <bridan2005@t-online.de>
2022-01-23 21:09:32 +0100

    Add comments

--- PXLed/Config.cs
+++ PXLed/Config.cs
@@ -11,6 +11,8 @@ namespace PXLed
         public Config(string fileName)
         {
             FileName = fileName;
+            
+            // Load config when first constructed
             Load();
         }
 
@@ -20,8 +22,11 @@ namespace PXLed
 
         public T GetData<T>()
         {
+            // JSON property name is equal to actual class name
+            // This makes it so that there can only be one of every class per config file
             string key = typeof(T).FullName!;
 
+            // Return the property if it's in the config already, otherwise, return a new instance of the property
             if (dataDictionary!.ContainsKey(key))
             {
                 JsonElement jsonElement = (JsonElement)dataDictionary[key];
@@ -34,8 +39,11 @@ namespace PXLed
 
         public void SetData<T>(T data)
         {
+            // JSON property name is equal to actual class name
+            // This makes it so that there can only be one of every class per config file
             string key = typeof(T).FullName!;
 
+            // Overwrite property if already in config, otherwise, add it to config.
             if (dataDictionary!.ContainsKey(key))
             {
                 dataDictionary[key] = data!;
@@ -47,6 +55,8 @@ namespace PXLed
 
         public void Load()
         {
+            // Read file if it exists
+            // If not, create a new empty config dictionary
             if (File.Exists(FileName))
             {
                 string json = File.ReadAllText(FileName);
@@ -59,6 +69,7 @@ namespace PXLed
 
         public void Save()
         {
+            // Write data to file
             string json = JsonSerializer.Serialize(dataDictionary, new JsonSerializerOptions { WriteIndented = true });
             File.WriteAllText(FileName, json);
         }
