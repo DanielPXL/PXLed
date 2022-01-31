45eb6facb025dea909f8f145c2c8ff63405cdd15
DanielPXL <bridan2005@t-online.de>
2022-01-31 15:51:29 +0100

    Switched from System.Text.Json to Newtonsoft.Json

--- PXLed/Config.cs
+++ PXLed/Config.cs
@@ -2,7 +2,8 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Reflection;
-using System.Text.Json;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace PXLed
 {
@@ -24,13 +25,12 @@ namespace PXLed
         {
             // JSON property name is equal to actual class name
             // This makes it so that there can only be one of every class per config file
-            string key = typeof(T).FullName!;
+            string key = typeof(T).AssemblyQualifiedName!;
 
             // Return the property if it's in the config already, otherwise, return a new instance of the property
             if (dataDictionary!.ContainsKey(key))
             {
-                JsonElement jsonElement = (JsonElement)dataDictionary[key];
-                return jsonElement.Deserialize<T>()!;
+                return (T)dataDictionary[key];
             } else
             {
                 return Activator.CreateInstance<T>();
@@ -41,7 +41,7 @@ namespace PXLed
         {
             // JSON property name is equal to actual class name
             // This makes it so that there can only be one of every class per config file
-            string key = typeof(T).FullName!;
+            string key = typeof(T).AssemblyQualifiedName!;
 
             // Overwrite property if already in config, otherwise, add it to config.
             if (dataDictionary!.ContainsKey(key))
@@ -55,22 +55,34 @@ namespace PXLed
 
         public void Load()
         {
-            // Read file if it exists
-            // If not, create a new empty config dictionary
+            dataDictionary = new Dictionary<string, object>();
+
+            // Create new empty data dictionary
+            // Populate it with data from config file if it exists
             if (File.Exists(FileName))
             {
                 string json = File.ReadAllText(FileName);
-                dataDictionary = JsonSerializer.Deserialize<Dictionary<string, object>>(json);
-            } else
-            {
-                dataDictionary = new Dictionary<string, object>();
+                Dictionary<string, object>? jsonContent = JsonConvert.DeserializeObject<Dictionary<string, object>>(json);
+                foreach (KeyValuePair<string, object> kvp in jsonContent!)
+                {
+                    Type? type = Type.GetType(kvp.Key);
+                    if (type != null)
+                    {
+                        JObject jsonObject = (JObject)kvp.Value;
+                        object? savedValue = jsonObject.ToObject(type);
+                        if (savedValue != null)
+                        {
+                            dataDictionary![kvp.Key] = savedValue;
+                        }
+                    }
+                }
             }
         }
 
         public void Save()
         {
             // Write data to file
-            string json = JsonSerializer.Serialize(dataDictionary, new JsonSerializerOptions { WriteIndented = true });
+            string json = JsonConvert.SerializeObject(dataDictionary, Formatting.Indented);
             File.WriteAllText(FileName, json);
         }
     }
