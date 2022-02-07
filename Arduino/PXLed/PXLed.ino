fa3474ab06ff891507bb73fb713f7d18c6d71602
DanielPXL <bridan2005@t-online.de>
2022-02-07 19:30:35 +0100

    Optimize arduino code

--- Arduino/PXLed/PXLed.ino
+++ Arduino/PXLed/PXLed.ino
@@ -4,35 +4,24 @@
 #define NUM_LEDS 91
 #define LED_PIN 13
 
-CRGB leds[NUM_LEDS];
+CRGB leds[NUM_REAL_LEDS];
 
-byte buffer[NUM_LEDS * 3 + 1];
+byte buffer[3];
 int i;
+int c;
 
 bool startRead = false;
 bool readComplete = false;
 
 void setup() {
   Serial.begin(500000);
-  
+
   FastLED.addLeds<WS2812B, LED_PIN, GRB>(leds, NUM_REAL_LEDS);
   FastLED.setBrightness(60);
 }
 
 void loop() {
-  if (readComplete) {
-    for (int o = 0; o < NUM_LEDS; o++) {
-      byte r = buffer[o * 3];
-      byte g = buffer[(o * 3) + 1];
-      byte b = buffer[(o * 3) + 2];
 
-      leds[o + NUM_REAL_LEDS - NUM_LEDS] = CRGB(r, g, b);
-    }
-    
-    FastLED.show();
-    readComplete = false;
-    Serial.write((byte)100);
-  }
 }
 
 void serialEvent() {
@@ -40,16 +29,24 @@ void serialEvent() {
     byte serialIn = (byte)Serial.read();
 
     if (startRead == true) {
-      buffer[i] = serialIn;
-      
-      if (buffer[i] == 255) {
-        readComplete = true;
+      if (serialIn == 255) {
         startRead = false;
+
+        FastLED.show();
+        Serial.write((byte)100);
+
         i = 0;
       } else {
-        i++;
+        buffer[c] = serialIn;
+
+        c++;
+        if (c == 3) {
+          leds[i + NUM_REAL_LEDS - NUM_LEDS] = CRGB(buffer[0], buffer[1], buffer[2]);
+          c = 0;
+          i++;
+        }        
       }
-    }   
+    }
 
     if (serialIn == 254) {
       startRead = true;
