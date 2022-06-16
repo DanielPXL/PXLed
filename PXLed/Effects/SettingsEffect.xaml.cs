b9a3e954aff51f39169950d437f3cece4e262184
DanielPXL <bridan2005@t-online.de>
2022-06-16 22:48:24 +0200

    WiFi/USB specific controls now disable when not meant to be used

--- PXLed/Effects/SettingsEffect.xaml.cs
+++ PXLed/Effects/SettingsEffect.xaml.cs
@@ -20,6 +20,8 @@ namespace PXLed.Effects
         public SettingsEffect()
         {
             InitializeComponent();
+
+            useWiFiCheckBox.Click += (s, e) => UpdateControlsIfUsingWifi();
         }
 
         public string DisplayName => "Settings";
@@ -49,6 +51,8 @@ namespace PXLed.Effects
             useWiFiCheckBox.IsChecked = data.UseWiFi;
             ipBox.Text = data.DeviceIP;
             portBox.Value = data.DevicePort;
+
+            UpdateControlsIfUsingWifi();
         }
 
         public void OnStop()
@@ -64,6 +68,27 @@ namespace PXLed.Effects
             };
 
             App.Config.SetData(data);
+        }
+
+        void UpdateControlsIfUsingWifi()
+        {
+            // If using wifi, enable ip and port boxes and disable port name box and port box
+            if (useWiFiCheckBox.IsChecked ?? false)
+            {
+                ipBox.IsEnabled = true;
+                portBox.IsEnabled = true;
+
+                portNameBox.IsEnabled = false;
+                baudRateBox.IsEnabled = false;
+            }
+            else
+            {
+                ipBox.IsEnabled = false;
+                portBox.IsEnabled = false;
+
+                portNameBox.IsEnabled = true;
+                baudRateBox.IsEnabled = true;
+            }
         }
     }
 
